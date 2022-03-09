4c6b685cb408c2b83d5cea59cd448d848daa7c33
Jab125 <67534807+Jab125@users.noreply.github.com>
2022-03-09 21:54:35 +1100

    Add Events

---
+++ src/main/java/com/affehund/voidtotem/api/VoidTotemEvent.java
@@ -0,0 +1,40 @@
+package com.affehund.voidtotem.api;
+
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraftforge.eventbus.api.Cancelable;
+import net.minecraftforge.eventbus.api.Event;
+
+
+/**
+ * Cancel the event to disable normal functionality
+ */
+@Cancelable
+public class VoidTotemEvent extends Event {
+    private final ItemStack itemStack;
+    private final LivingEntity entity;
+    private final DamageSource source;
+    public VoidTotemEvent(ItemStack itemStack, LivingEntity entity, DamageSource source) {
+        this.itemStack = itemStack;
+        this.entity = entity;
+        this.source = source;
+    }
+
+    public ItemStack getItemStack() {
+        return itemStack;
+    }
+
+    public LivingEntity getEntity() {
+        return entity;
+    }
+
+    public DamageSource getSource() {
+        return source;
+    }
+
+    @Override
+    public boolean isCancelable() {
+        return true;
+    }
+}
